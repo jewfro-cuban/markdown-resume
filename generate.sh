7355db3d21fe85374f1b41cae7a3695805b5ef51
ostools <ostools@ostools>
2020-12-01 08:29:10 +0200

    seems that a standalone flag is requires (-s)

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-pandoc -o resume.html -c resume-css-stylesheet.css resume.md
+pandoc -o resume.html -c resume-css-stylesheet.css -s resume.md
 wkhtmltopdf resume.html resume.pdf 
 pandoc -o resume.docx --reference-doc=resume-docx-reference.docx resume.md
